4ba321588bd993d2c05f39dd42588096c65c945f
ilya.komolkin <ilya.komolkin@jetbrains.com>
2015-08-31 19:12:25 +0300

    heap sort spaces

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -6,6 +6,9 @@ public class HeapSort {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
+
+
+
     int[] array = new int[n];
     for (int i = 0; i < n; i++) {
       array[i] = scanner.nextInt();
