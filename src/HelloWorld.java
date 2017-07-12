4ca862095bbb97cd6af4ae2be6eb13d1206d2187
iluxa-com <iluxa_com@mail.ru>
2017-07-12 17:05:36 +0300

    draft test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,6 +13,8 @@ public class HelloWorld {
     System.out.println("one more commit");
     System.out.println("I need one more commit, for 'more' button tests.");
     System.out.println("It's not over yet.");
+
+
     System.out.println("Oh, it's so boring.");
     System.out.println("It's been so lonely here.");
     ////
